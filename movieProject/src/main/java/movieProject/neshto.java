71777549a3b0d87d8eb7ec0aaa915c423c0e1375
0hakan <hakan.nihat@mail.bg>
2022-03-23 21:49:57 +0200

    Създаване на проект
    Създадох проекта с необходимите инструменти

--- movieProject/src/main/java/movieProject/neshto.java
+++
@@ -1,6 +0,0 @@
-package movieProject;
-
-public class neshto {
-public int hello;
-private int chislo;
-}
---
+++ src/common/Constants.java
@@ -0,0 +1,8 @@
+package common;
+
+public class Constants {
+	public static class View{
+		public static final String EXAMPLE ="/movieProject/bin/application/MovieApp.fxml";
+	}
+
+}
